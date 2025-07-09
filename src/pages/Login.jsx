bc671d7301de1f34d7bdcd5a9a21abedb52ca655
Liviu Bibire <bibire.liviu@gmail.com>
2025-07-09 10:06:47 +0300

    add Firebase config with environment variables for auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { auth } from "../firebase";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
